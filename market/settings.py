f0aceea51054d30c15ac8467370f59cd686b87c7
NIKOROSS <artemsorokin652@gmail.com>
2025-03-13 15:48:25 +0300

    Обновлены настройки базы данных MySQL

--- market/settings.py
+++ market/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["tutriba.ru", "www.tutriba.ru"]
 
 
 # Application definition
